4650e481c76f4c9374f34e5392fc72eceac233cb
Sinon <lidmuse@gmail.com>
2025-02-12 15:14:40 +0800

    use 2000

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     };
 
     let mut body = Vec::new();
-    let timeout = Duration::from_millis(1500);
+    let timeout = Duration::from_millis(2000);
 
     // Create a request with a timeout
     let mut request = request::Request::new(&uri);
